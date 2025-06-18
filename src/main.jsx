100d86cd0cfd38a4268bfd3f5eb583643915ee00
Ro-off <romansk2001@gmail.com>
2025-06-18 18:45:09 +0300

    Add react-router for routing and update package dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { BrowserRouter, Routes, Route, NavLink } from "react-router";
 import App from './App.jsx'
 import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
 
+const routes = [
+  { path: "/", label: "Home", element: <App /> }
+]
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    <nav style={{ position: "fixed", top: 0, display: "flex", gap: 15, margin: 30 }}>
+      {routes.map((route) => (
+        <NavLink key={route.path} to={route.path} end>
+          {route.label}
+        </NavLink>
+      ))}
+    </nav>
+
+        <Routes>
+              {
+                routes.map((route) => (
+                  <Route key={route.path} path={route.path} element={route.element} />
+                ))
+              }
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
